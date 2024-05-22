6f0d3f95bef9b0528484a4825534e304941eeecf
Pranay Virat Dontula <52294386+pranayvirat@users.noreply.github.com>
2024-05-22 06:24:49 +0000

    Remove Duplicates from Sorted List(Easy)

---
+++ 20 May 2024/21 May 2024/Remove Duplicates from Sorted List - Leet Code/Solution.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+
+        ListNode res = head;
+
+        while(res!=null && res.next!=null){
+            if(res.val == res.next.val){
+                res.next = res.next.next;
+            }
+            else{
+                res = res.next;
+            }
+        }
+        return head;
+
+
+        
+    }
+}
